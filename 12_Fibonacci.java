9c7d08014135df9cf26b7dc647a097c92d91ab37
ImamSohaib <sohaibimam1594@gmail.com>
2026-04-28 20:17:58 +0530

    Create 12_Fibonacci.java

---
+++ 12_Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter terms: ");
+        int n = sc.nextInt();
+
+        int x = 0, y = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(x + " ");
+            int z = x + y;
+            x = y;
+            y = z;
+        }
+    }
+}

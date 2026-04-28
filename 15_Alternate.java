a8dd8d758432a97e5ea296da2e7ed95ff986b2fc
ImamSohaib <sohaibimam1594@gmail.com>
2026-04-28 20:21:40 +0530

    Create 15_Alternate.java

---
+++ 15_Alternate.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Alternate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int a[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        System.out.println("Alternate elements:");
+        for (int i = 0; i < n; i += 2)
+            System.out.print(a[i] + " ");
+    }
+}

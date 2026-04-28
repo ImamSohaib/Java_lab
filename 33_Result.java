a6ae5eedab72948f48f3324eece28823b81807bb
ImamSohaib <sohaibimam1594@gmail.com>
2026-04-28 20:43:54 +0530

    Create 33_Result.java

---
+++ 33_Result.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+interface Exam {
+    void percent_cal();
+}
+
+class Student {
+    String name;
+    int roll_no;
+    float marks1, marks2;
+
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter roll number: ");
+        roll_no = sc.nextInt();
+
+        System.out.print("Enter marks1: ");
+        marks1 = sc.nextFloat();
+
+        System.out.print("Enter marks2: ");
+        marks2 = sc.nextFloat();
+    }
+
+    void show() {
+        System.out.println("\n--- Student Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll_no);
+        System.out.println("Marks1: " + marks1);
+        System.out.println("Marks2: " + marks2);
+    }
+}
+
+class Result extends Student implements Exam {
+    float percentage;
+
+    public void percent_cal() {
+        percentage = (marks1 + marks2) / 2;
+    }
+
+    void display() {
+        System.out.println("Percentage: " + percentage + "%");
+    }
+}
+
+public class ResultMain {
+    public static void main(String[] args) {
+        Result r = new Result();
+
+        r.getData();
+        r.show();
+        r.percent_cal();
+        r.display();
+    }
+}
